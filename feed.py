633c2dbeb9511e420e35c4c2193f6f9757437709
Riley Raschke <riley@rrappsdev.com>
2018-12-27 22:50:26 -0600

    No warnings/errors. Massaging stuff

--- feed.py
+++ feed.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python3
 
+import traceback
+
 import websocket
+import json
+
+class OrderBook:
+    def __init__(self):
+        self.bidBook = {}
+        self.askBook = {}
+        self.last = 0.0
+        self.lastSide = ""
+        self.lastAmount = 0.0
+        self.bid = 0.0
+        self.bidSize = 0.0
+        self.ask = 0.0
+        self.askSize = 0.0
+
+    def addEvent(self, event):
+        if event['type'] == 'trade':
+            self.last = event['price']
+            self.lastSide = event['makerSide']
+            self.lastAmount = event['amount']
+
+        if event['type'] == 'change':
+            try:
+                if event['side'] == 'bid':
+                    if not str(event['price']) in self.bidBook:
+                        self.bidBook[str(event['price'])] = 0.0
+                    self.bidBook[str(event['price'])] += float(event['delta'])
+                elif event['side'] == 'ask':
+                    if not str(event['price']) in self.askBook:
+                        self.askBook[str(event['price'])] = 0.0
+                    self.askBook[str(event['price'])] = float(event['delta'])
+            except Exception:
+                traceback.print_exc()
+
+    def printStats(self):
+        print( "Last: " + str(self.last) + " LastSide: " + self.lastSide + " LastAmount: " + str(self.lastAmount)  )
+
+orderBook = OrderBook()
 
 def on_message(ws, message):
-    print( message )
+    feedData = json.loads( message );
+    #if( feedData['events']
+    #print( str(feedData['socket_sequence']) + " " + str(feedData['events']) )
+    for event in feedData['events']:
+        orderBook.addEvent( event )
+        orderBook.printStats()
 
-ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/btcusd",
-                                on_message=on_message)
+ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/btcusd", on_message=on_message)
 
 ws.run_forever(ping_interval=5)
 
