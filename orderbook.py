53b21bd8a3e86f631f2821f018bd31a4058836da
Riley Raschke <riley@rrappsdev.com>
2018-12-28 13:39:56 -0600

    Added dept of displayed stuff

--- orderbook.py
+++ orderbook.py
@@ -1,19 +1,24 @@
 
 import traceback
 import pprint
+from functools import reduce
 
 class OrderBook:
     def __init__(self, depthPercent):
         self.depthPercent = depthPercent
         self.bidBook = {}
         self.askBook = {}
+        self.bidDepth = {}
+        self.askDepth = {}
         self.last = 0.0
         self.lastSide = ""
         self.lastAmount = 0.0
         self.bid = 0.0
         self.bidSize = 0.0
+        self.bidDepthSize = 0.0
         self.ask = 0.0
         self.askSize = 0.0
+        self.askDepthSize = 0.0
 
     def addEvent(self, event):
         if event['type'] == 'trade':
@@ -36,9 +41,21 @@ class OrderBook:
                 if self.bidBook:
                     self.bid = max( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0.00001, self.bidBook.items())).keys()) );
                     if self.bid > 0.00001: self.bidSize = self.bidBook[ "{:5.2f}".format(self.bid) ]
+                    self.bidDepth = dict( filter(
+                        lambda x:
+                            float(x[0]) > self.bid - self.bid*self.depthPercent and x[1] > 0.00001,
+                        self.bidBook.items()
+                    ))
+                    self.bidDepthSize = reduce( lambda x, y: x+y, self.bidDepth.values() );
                 if self.askBook:
                     self.ask = min( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0.00001, self.askBook.items())).keys()) );
                     if self.ask > 0.00001: self.askSize = self.askBook[ "{:5.2f}".format(self.ask) ]
+                    self.askDepth = dict( filter(
+                        lambda x:
+                            float(x[0]) < self.ask + self.ask*self.depthPercent and x[1] > 0.00001,
+                        self.askBook.items()
+                    ))
+                    self.askDepthSize = reduce( lambda x, y: x+y, self.askDepth.values() );
 
             except Exception:
                 traceback.print_exc()
@@ -53,22 +70,16 @@ class OrderBook:
 
     def printStats(self):
         try:
-            pprint.pprint( dict( filter(
-                lambda x:
-                    float(x[0]) > self.bid - self.ask*self.depthPercent and x[1] > 0.00001,
-                self.bidBook.items()
-            )))
+            pprint.pprint( self.bidDepth );
+            print( "bidDepth: " + "{:5.1f}".format(self.bidDepthSize) )
             print( "bid: " + "{:5.2f}".format(self.bid) + " x " + "{:5.5f}".format(self.bidSize) )
 
             if self.last > 0: self.printLast()
             else: print("spread: " + "{:5.5f}".format(self.ask-self.bid) )
 
             print( "ask: " + "{:5.2f}".format(self.ask) + " x " + "{:5.5f}".format(self.askSize) )
-            pprint.pprint( dict( filter(
-                lambda x:
-                    float(x[0]) < self.ask + self.ask*self.depthPercent and x[1] > 0.00001,
-                self.askBook.items()
-            )))
+            print( "askDepth: " + "{:5.1f}".format(self.askDepthSize) )
+            pprint.pprint( self.askDepth );
         except Exception:
             traceback.print_exc()
 
