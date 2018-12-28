7c6e8293d4d756211456639172ed99c6635006c6
Riley Raschke <riley@rrappsdev.com>
2018-12-28 11:52:07 -0600

    Well thats kinda too fun...

--- feed.py
+++ feed.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-TICKER_SYMBOL='btcusd'
+DEFAULT_TICKER_SYMBOL='btcusd'
 
 import traceback
 import pprint
 
 import websocket
 import json
+
+import argparse
+opts = argparse.ArgumentParser(description="Display Gemini data feed in terminal.")
+opts.add_argument('-t', '--ticker', default=DEFAULT_TICKER_SYMBOL, help='Ticker string. ex: btcusd ethbtc bchusd')
+args = opts.parse_args()
 
 class OrderBook:
     def __init__(self):
@@ -28,6 +33,7 @@ class OrderBook:
 
         if event['type'] == 'change':
             try:
+
                 if event['side'] == 'bid':
                     if not str(event['price']) in self.bidBook:
                         self.bidBook[str(event['price'])] = 0.0
@@ -36,25 +42,38 @@ class OrderBook:
                     if not str(event['price']) in self.askBook:
                         self.askBook[str(event['price'])] = 0.0
                     self.askBook[str(event['price'])] += float(event['delta'])
+
+                if self.bidBook:
+                    self.bid = max( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0, self.bidBook.items())).keys()) );
+                    if self.bid > 0: self.bidSize = self.bidBook[ "{:5.2f}".format(self.bid) ]
+                if self.askBook:
+                    self.ask = min( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0, self.askBook.items())).keys()) );
+                    if self.ask > 0: self.askSize = self.askBook[ "{:5.2f}".format(self.ask) ]
+
             except Exception:
                 traceback.print_exc()
+
+    def printLast(self):
+        print(
+            "Last: " + str(self.last) +
+            " spread: " + str(self.ask-self.bid) +
+            " last_amt: " + str(self.lastAmount) +
+            " last_side: " + self.lastSide
+        )
 
     def printStats(self):
-        print(
-            "\nLast: " + str(self.last) + "\n" +
-            "LastSide: " + self.lastSide + " \n" +
-            "LastAmount: " + str(self.lastAmount)
-        )
-        if self.last > 0:
-            try:
-                print("BidBook: ")
-                pprint.pprint( dict(filter( lambda x: float(x[0]) > self.last-15 and x[1] > 0, self.bidBook.items() )) )
-                print("AskBook: ")
-                pprint.pprint( dict(filter( lambda x: float(x[0]) < self.last+15 and x[1] > 0, self.askBook.items() )) )
-            except Exception:
-                traceback.print_exc()
+        try:
+            pprint.pprint( dict(filter( lambda x: float(x[0]) > self.bid-7.5 and x[1] > 0, self.bidBook.items() )) )
+            print( "bid: " + str(self.bid) + "x" + str(self.bidSize) )
 
+            if self.last > 0: self.printLast()
+            else: print("spread: " + str(self.ask-self.bid) )
 
+            print( "ask: " + str(self.ask) + "x" + str(self.askSize) )
+            pprint.pprint( dict(filter( lambda x: float(x[0]) < self.ask+7.5 and x[1] > 0, self.askBook.items() )) )
+            print("\n")
+        except Exception:
+            traceback.print_exc()
 
 orderBook = OrderBook()
 
@@ -66,7 +85,7 @@ def on_message(ws, message):
         orderBook.addEvent( event )
         orderBook.printStats()
 
-ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/" + TICKER_SYMBOL, on_message=on_message)
+ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/" + args.ticker, on_message=on_message)
 
 ws.run_forever(ping_interval=5)
 
