b9d4bbcceef188d3b0f48b1d5714043366a1015f
Riley Raschke <riley@rrappsdev.com>
2018-12-31 15:23:27 -0600

    Currently no warnings/errors

--- feed.py
+++ feed.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
+import sys
+import time
+import argparse
+import websocket
+import json
+from orderbook import OrderBook
+
 DEFAULT_TICKER_SYMBOL='btcusd'
 DEPTH_PERCENT_DEFAULT=0.01
 
-import traceback
-import pprint
-
-import websocket
-import json
-
-from orderbook import OrderBook
-
-import argparse
 opts = argparse.ArgumentParser(description="Display Gemini data feed in terminal.")
 
 opts.add_argument('-t', '--ticker', default=DEFAULT_TICKER_SYMBOL, help='Ticker string. ex: btcusd ethbtc bchusd')
@@ -22,12 +20,16 @@ args = opts.parse_args()
 orderBook = OrderBook(args.depthPercent)
 
 def on_message(ws, message):
-    feedData = json.loads( message );
-    #if( feedData['events']
-    #print( str(feedData['socket_sequence']) + " " + str(feedData['events']) )
+    #sys.stderr.write( message )
+    feedData = json.loads( message )
+    ts = 0
+    if 'timestamp' in feedData.keys(): ts = feedData['timestamp']
+    else: ts = int(time.time())
+
     for event in feedData['events']:
-        orderBook.addEvent( event )
-        orderBook.printStats()
+        orderBook.addEvent( event, ts )
+
+    orderBook.printStats()
 
 ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/" + args.ticker, on_message=on_message)
 
--- orderbook.py
+++ orderbook.py
@@ -2,6 +2,7 @@
 import traceback
 import pprint
 from functools import reduce
+from datetime import datetime
 
 class OrderBook:
     def __init__(self, depthPercent):
@@ -13,22 +14,25 @@ class OrderBook:
         self.last = 0.0
         self.lastSide = ""
         self.lastAmount = 0.0
+        self.lastTickTS = 0
         self.bid = 0.0
+        self.bidTs = 0
         self.bidSize = 0.0
         self.bidDepthSize = 0.0
         self.ask = 0.0
+        self.askTs = 0
         self.askSize = 0.0
         self.askDepthSize = 0.0
 
-    def addEvent(self, event):
+    def addEvent(self, event, timestamp):
         if event['type'] == 'trade':
             self.last = float(event['price'])
             self.lastAmount = float(event['amount'])
             self.lastSide = event['makerSide']
+            self.lastTickTS = int(timestamp)
 
         if event['type'] == 'change':
             try:
-
                 if event['side'] == 'bid':
                     if not str(event['price']) in self.bidBook:
                         self.bidBook[ "{:5.2f}".format(float(event['price'])) ] = 0.0
@@ -39,23 +43,34 @@ class OrderBook:
                     self.askBook[ "{:5.2f}".format(float(event['price'])) ] += float(event['delta'])
 
                 if self.bidBook:
-                    self.bid = max( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0.00001, self.bidBook.items())).keys()) );
-                    if self.bid > 0.00001: self.bidSize = self.bidBook[ "{:5.2f}".format(self.bid) ]
-                    self.bidDepth = dict( filter(
-                        lambda x:
-                            float(x[0]) > self.bid - self.bid*self.depthPercent and x[1] > 0.00001,
-                        self.bidBook.items()
-                    ))
-                    self.bidDepthSize = reduce( lambda x, y: x+y, self.bidDepth.values() );
+                    viableBids = dict(filter(lambda x: x[1] > 0.00001, self.bidBook.items()))
+                    if viableBids:
+                        self.bid = max( map(lambda x: float(x), viableBids.keys()) );
+                        if self.bid > 0.00001:
+                            self.bidSize = self.bidBook[ "{:5.2f}".format(self.bid) ]
+                            self.bidTs = int(timestamp)
+
+                        self.bidDepth = dict( filter(
+                            lambda x:
+                                float(x[0]) > self.bid - self.bid*self.depthPercent,
+                            viableBids.items()
+                        ))
+                        self.bidDepthSize = reduce( lambda x, y: x+y, self.bidDepth.values() );
+
                 if self.askBook:
-                    self.ask = min( map(lambda x: float(x), dict(filter(lambda x: x[1] > 0.00001, self.askBook.items())).keys()) );
-                    if self.ask > 0.00001: self.askSize = self.askBook[ "{:5.2f}".format(self.ask) ]
-                    self.askDepth = dict( filter(
-                        lambda x:
-                            float(x[0]) < self.ask + self.ask*self.depthPercent and x[1] > 0.00001,
-                        self.askBook.items()
-                    ))
-                    self.askDepthSize = reduce( lambda x, y: x+y, self.askDepth.values() );
+                    viableAsks = dict(filter(lambda x: x[1] > 0.00001, self.askBook.items()))
+                    if viableAsks:
+                        self.ask = min( map(lambda x: float(x), viableAsks.keys()) );
+                        if self.ask > 0.00001:
+                            self.askSize = self.askBook[ "{:5.2f}".format(self.ask) ]
+                            self.askTs = int(timestamp)
+
+                        self.askDepth = dict( filter(
+                            lambda x:
+                                float(x[0]) < self.ask + self.ask*self.depthPercent,
+                            viableAsks.items()
+                        ))
+                        self.askDepthSize = reduce( lambda x, y: x+y, self.askDepth.values() );
 
             except Exception:
                 traceback.print_exc()
@@ -64,20 +79,25 @@ class OrderBook:
         print(
             "Last: " + "{:5.2f}".format(self.last) +
             " x " + "{:5.5f}".format(self.lastAmount) +
-            " last_side: " + self.lastSide +
-            " spread: " + "{:5.5f}".format(self.ask-self.bid)
+            "; last_side: " + self.lastSide +
+            "; spread: " + "{:5.5f}".format(self.ask-self.bid) +
+            "; " + datetime.utcfromtimestamp(self.lastTickTS).strftime('%H:%M:%S')
         )
 
     def printStats(self):
         try:
             pprint.pprint( self.bidDepth );
             print( "bidDepth: " + "{:5.1f}".format(self.bidDepthSize) )
-            print( "bid: " + "{:5.2f}".format(self.bid) + " x " + "{:5.5f}".format(self.bidSize) )
+            print( "bid: " + "{:5.2f}".format(self.bid) + " x " + "{:5.5f}".format(self.bidSize)
+                +" @ " + datetime.utcfromtimestamp(self.bidTs).strftime('%H:%M:%S')
+            )
 
             if self.last > 0: self.printLast()
             else: print("spread: " + "{:5.5f}".format(self.ask-self.bid) )
 
-            print( "ask: " + "{:5.2f}".format(self.ask) + " x " + "{:5.5f}".format(self.askSize) )
+            print( "ask: " + "{:5.2f}".format(self.ask) + " x " + "{:5.5f}".format(self.askSize)
+                + " @ " + datetime.utcfromtimestamp(self.askTs).strftime('%H:%M:%S')
+            )
             print( "askDepth: " + "{:5.1f}".format(self.askDepthSize) )
             pprint.pprint( self.askDepth );
         except Exception:
